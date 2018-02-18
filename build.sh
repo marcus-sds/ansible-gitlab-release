fa0c08acc15b5bb5e908ed2ba66221e59647ff6a
Ubuntu <ubuntu@ip-10-51-32-202.us-west-2.compute.internal>
2018-02-18 04:50:49 +0000

    .

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-bosh create-release --tarball=ansible-gitlab.gz
-bosh -e $TARGET upload-release ./ansible-gitlab.gz
+bosh create-release --tarball=../ansible-gitlab.gz --force
+bosh -e $TARGET upload-release ../ansible-gitlab.gz
